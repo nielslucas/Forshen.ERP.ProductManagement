67b2e5968583cbd6c2e4092a6194be13b580a693
niels <niels_lucas@hotmail.com>
2025-07-16 21:12:45 +0200

    stuff

--- Src/Forshen.ERP.ProductManagement/Infrastructure/AppDbContextFactory.cs
+++ Src/Forshen.ERP.ProductManagement/Infrastructure/AppDbContextFactory.cs
@@ -8,7 +8,8 @@ public class AppDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
     public AppDbContext CreateDbContext(string[] args)
     {
         var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
-        optionsBuilder.UseSqlServer("Server= localhost; User Id = sa; Password = troef23@sycon2008; Database = Forshen.ERP.ProductManagement; Trusted_Connection=True; TrustServerCertificate=True");
+        //optionsBuilder.UseSqlServer("Server= localhost; User Id = sa; Password = troef23@sycon2008; Database = Forshen.ERP.ProductManagement; Trusted_Connection=True; TrustServerCertificate=True");
+        optionsBuilder.UseSqlServer("Server=localhost;Database=Forshen.ERP.ProductManagement;Trusted_Connection=True;TrustServerCertificate=True");
 
         return new AppDbContext(optionsBuilder.Options);
     }
---
+++ Src/Forshen.ERP.ProductManagement/Migrations/20250713101523_Update.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Forshen.ERP.ProductManagement.Migrations
+{
+    /// <inheritdoc />
+    public partial class Update : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Dimensions",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(128)", maxLength: 128, nullable: false),
+                    CreatedAt = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: false),
+                    UpdatedAt = table.Column<DateTimeOffset>(type: "datetimeoffset", maxLength: 128, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Dimensions", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "DimensionValues",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    DimensionId = table.Column<int>(type: "int", nullable: false),
+                    Value = table.Column<string>(type: "nvarchar(128)", maxLength: 128, nullable: false),
+                    CreatedAt = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: false),
+                    UpdatedAt = table.Column<DateTimeOffset>(type: "datetimeoffset", maxLength: 128, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DimensionValues", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_DimensionValues_Dimensions_DimensionId",
+                        column: x => x.DimensionId,
+                        principalTable: "Dimensions",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DimensionValues_DimensionId",
+                table: "DimensionValues",
+                column: "DimensionId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DimensionValues");
+
+            migrationBuilder.DropTable(
+                name: "Dimensions");
+        }
+    }
+}
